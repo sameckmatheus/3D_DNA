e62d578835859974f6b0293f45691148a47d8a62
sameckmatheus <sameckmatheuspro@gmail.com>
2023-08-16 21:59:01 -0300

    Update: Change DNA Structure

--- dna.py
+++ dna.py
@@ -3,3 +3,9 @@ from Bio.PDB import PDBParser
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+
+def load_dna_strcture(pdb_file):
+  paser = PDBParser(quit=true)
+  structure = paser.get_structure('DNA', pdb_file)
+  model = structure[0]
+  return model
