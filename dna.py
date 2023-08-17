5d02005c76c2dfa4b3565a363ccf434b12944ec5
sameckmatheus <sameckmatheuspro@gmail.com>
2023-08-16 22:16:30 -0300

    Update: Change DNA Render

--- dna.py
+++ dna.py
@@ -9,3 +9,25 @@ def load_dna_strcture(pdb_file):
   structure = paser.get_structure('DNA', pdb_file)
   model = structure[0]
   return model
+
+# Inicializando a janela OpenGL
+def draw_dna_model(model):
+  glutInit()
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+  glutCreateWindow("Representação do DNA humano")
+
+  # Inicializando a renderização da janela
+  def render():
+    glClear(GL_COLOR_BUFFER_CLEAR)
+    glRotatef(1, 3, 1, 1)
+
+    glLineWidth(2.0)
+    glBegin(GL_LINES)
+    for chain in model.get_chains():
+      for residue in chain:
+        for atom in residue:
+          if atom.get_name() == "p":
+            x, y, z = atom.get_coord()
+            glVertex3f(x, y, z)
+    glEnd()
+    glFlush()
