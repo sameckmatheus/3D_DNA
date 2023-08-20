991dfa176c1cac4a14c7c50f534d5d5a672aa9da
sameckmatheus <sameckmatheuspro@gmail.com>
2023-08-20 08:13:30 -0300

    Update: Defining a function to load the structure of DNA from a PDB file

--- test/dna-test.py
+++ test/dna-test.py
@@ -4,7 +4,7 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
-# Defininfo as conts para visualização (janela/camera)
+# Definindo as conts para visualização (janela/camera)
 WINDOW_TITLE = "DNA Humano"
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
@@ -15,3 +15,11 @@ CAMERA_POS = (0, 0, -15)
 CAMERA_TARGET = (0, 0, 0)
 CAMERA_UP = (0, 1, 0)
 
+# Definindo uma função para carregar a estrutura do DNA a partir de um arquivo PDB
+def load_dna_structure(pdb_file):
+  parser = PDBParser(QUIET=True)
+  structure = parser.get_structure('DNA', pdb_file)
+  model = structure[0]
+  return model
+
+# Definindo uma função para desenhar a estrutura do DNA usando linhas
