8a3aed876ce7347f70514834b8e55b3db969d446
sameckmatheus <sameckmatheuspro@gmail.com>
2023-08-20 08:07:42 -0300

    Update: Setting the conts for viewing (window/camera)

--- test/dna-test.py
+++ test/dna-test.py
@@ -4,3 +4,14 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
+# Defininfo as conts para visualização (janela/camera)
+WINDOW_TITLE = "DNA Humano"
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+FOV = 45
+NEAR = 1
+FAR = 50
+CAMERA_POS = (0, 0, -15)
+CAMERA_TARGET = (0, 0, 0)
+CAMERA_UP = (0, 1, 0)
+
